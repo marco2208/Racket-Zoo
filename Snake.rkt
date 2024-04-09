13ce7d00b3f0f8e5cb580106234b2d5a39236690
CollinGomillia <83978118+CollinGomillia@users.noreply.github.com>
2024-04-08 21:02:06 -0400

    Create Snake.rkt

---
+++ Snake.rkt
@@ -0,0 +1,25 @@
+#lang racket
+(require 2htdp/image)
+
+(define (draw-snake n)
+  ;; Define eye and eyes
+  (define eye (overlay (circle 2 'solid 'black) (circle 8 'solid 'white)))
+  (define eyes (beside eye eye))
+  
+  ;; Define tongue
+ (define tongue (overlay (rotate -15 (line 15 1 'red))
+                        (rotate 15 (line 15 1 'red))))
+
+  ;; Define head with eyes, tongue, and green circle
+  (define head (overlay/xy tongue 0 -20
+               (overlay/xy eyes 1 0
+                 (circle 20 'solid 'green))))
+  
+  ;; Define body segments as a list of green circles
+  (define body-segments (make-list n (circle 10 'solid 'green)))
+  
+  ;; Create body by aligning body segments vertically
+  (define body (apply beside/align "bottom" body-segments))
+  
+  ;; Overlay head on top of the aligned body
+ (overlay/align 'left "bottom" head body))

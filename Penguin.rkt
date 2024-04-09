61c44ddb3ef0cc224cde928d72afac9a531c57fb
CollinGomillia <83978118+CollinGomillia@users.noreply.github.com>
2024-04-09 18:43:07 -0400

    Update GoofyPenguin.rkt

--- Penguin.rkt
+++ Penguin.rkt
@@ -2,31 +2,44 @@
 
 (require 2htdp/image)
 
-; Define the parts of the penguin
-(define head (circle 20 "solid" "black"))
-(define beak (wedge 20 35 "solid" "orange"))
-(define body (ellipse 60 100 "solid" "black"))
-(define belly (ellipse 30 80 "solid" "grey"))
-(define feet (rectangle 30 10 "solid" "orange"))
-(define arm (ellipse 30 60 "solid" "black"))
+(define n 70)
 
-; Rotate the arms 
+(define head (circle (* 2 n) "solid" "black"))
+(define beak (triangle/sss 80 100 140 "solid" "orange"))
+(define body (ellipse (* 6 n) (* 10 n) "solid" "black"))
+(define belly (ellipse (* 3 n) (* 8 n) "solid" "grey"))
+(define tone (ellipse (* 1.9 n) (* 1.6 n) "solid" "yellow"))
+(define pupils (circle (* 0.25 n) "solid" "black"))
+(define eyes (circle (* 0.5 n) "solid" "white")) ; The white part of the eyes
+
+(define feet (rectangle (* 3 n) n "solid" "orange"))
+(define arm (ellipse (* 3 n) (* 6 n) "solid" "black"))
+; Rotate the arms
 (define left-arm (rotate 20 arm))
 (define right-arm (rotate -20 arm))
 
+
+(define googly-eye (overlay/offset pupils (* 0.2 n) 0 eyes)) 
+(define eyes-with-pupils (beside googly-eye googly-eye))
+(define head-with-eyes (overlay/align "center" "top" eyes-with-pupils head))
+
 ; Combine the head and beak
-(define head-beak (overlay/align "left" "middle" beak head))
-
-; Combine the body and belly
-(define body-belly (overlay/align "center" "middle" belly body))
+(define head-beak (overlay/offset beak (- (* 0.2 n)) (- (* 0.6 n)) head-with-eyes))
 
 
-; Adjust the offsets 
-(define body-arms (overlay/offset left-arm (- 30) 0 
-                  (overlay/offset right-arm 30 0 body-belly)))
+
+; Combine the belly and tone
+(define belly-tone (overlay/align "center" "top" tone belly))
+
+; Combine the body and belly-tone
+(define body-belly (overlay/align "center" "middle" belly-tone body))
+
+; Position the arms using overlay/offset to ensure they're visible
+(define body-arms (overlay/offset left-arm (- (* 3 n)) 0 
+                  (overlay/offset right-arm (* 3 n) 0 body-belly)))
 
 ; Combine the body-arms with feet
 (define body-arms-feet (overlay/align "center" "bottom" feet body-arms))
 
-; Position the parts to form the penguin silhouette
+
 (define penguin (above head-beak body-arms-feet))
